1e0f058c86bbcaed915e42e58458e20d739e8377
elvxk <pradiptaramadan9c@gmail.com>
2024-02-22 09:14:42 +0700

    🖖 Testing Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
-import Starter from "./starter/Starter";
+// import Starter from "./starter/Starter";
+import { useEffect, useState } from "react";
+import { getData } from "./api";
 
 function App() {
+  const [datas, setDatas] = useState([]);
+
+  useEffect(() => {
+    getData()
+      .then((res) => {
+        setDatas(res);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, []);
+
   return (
     <>
-      {/* Delete the starter component below and also delete the starter folder in src/starter */}
-      <Starter />
-      {/* Happy coding 🤘 */}
+      <div className="bg-violet-400 font-bold flex flex-wrap">
+        <h1>Hello</h1>
+      </div>
+      {/* <Starter /> */}
+      <div className="container mx-auto flex flex-col justify-center min-h-screen">
+        {datas.map(({ id, title }) => {
+          return (
+            <div className="" key={id}>
+              <p>{`${id}. ${title}`}</p>
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 }
---
+++ src/api.ts
@@ -0,0 +1,4 @@
+export const getData = async () => {
+  const response = await fetch("https://fakestoreapi.com/products");
+  return response.json();
+};
