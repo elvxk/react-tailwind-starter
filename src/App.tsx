fdd195bc5ca5a4e32f9df381824c4f0172d977af
elvxk <pradiptaramadan9c@gmail.com>
2024-02-22 09:45:53 +0700

    aw

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Starter from "./starter/Starter";
 function App() {
   return (
     <>
+      {/* sdsd */}
       {/* Delete the starter component below and also delete the starter folder in src/starter */}
       <Starter />
       {/* Happy coding 🤘 */}
